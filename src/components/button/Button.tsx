f1db33fdf07a25d5c787661d7e6fb5646d751b95
arjun-saseendran <arjunsaseendran@icloud.com>
2025-01-24 13:48:09 +0530

    add getButtonClasses

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,4 @@
-import classNames from "classnames";
+
 import ButtonProps from "./type";
 
 export const Button: React.FC<ButtonProps> = ({
@@ -12,22 +12,8 @@ export const Button: React.FC<ButtonProps> = ({
   ...restProps
 }) => {
   // Classes
-  const btnClasses = classNames(
-    "py-2 px-4 cursor-pointer rounded text-white font-bold",
-    {
-      "bg-primary  ": variant === "primary" && variantType === "solid",
-      "bg-secondary ": variant === "secondary" && variantType === "solid",
-      "bg-tertiary ": variant === "tertiary" && variantType === "solid",
-      "border-2 border-primary ":
-        variant === "primary" && variantType === "outline",
-      "border-2 border-secondary ":
-        variant === "secondary" && variantType === "outline",
-      "border-2 border-tertiary ":
-        variant === "tertiary" && variantType === "outline",
-      "cursor-not-allowed opacity-50": isLoading,
-    },
-    classes
-  );
+  
+
 
   return (
     <>
---
+++ src/components/button/utility.tsx
@@ -0,0 +1,27 @@
+import classNames from "classnames";
+
+
+const getButtonClasses = () =>{
+
+
+    const btnClasses = classNames(
+      "py-2 px-4 cursor-pointer rounded text-white font-bold",
+      {
+        "bg-primary  ": variant === "primary" && variantType === "solid",
+        "bg-secondary ": variant === "secondary" && variantType === "solid",
+        "bg-tertiary ": variant === "tertiary" && variantType === "solid",
+        "border-2 border-primary ":
+          variant === "primary" && variantType === "outline",
+        "border-2 border-secondary ":
+          variant === "secondary" && variantType === "outline",
+        "border-2 border-tertiary ":
+          variant === "tertiary" && variantType === "outline",
+        "cursor-not-allowed opacity-50": isLoading,
+      },
+      classes
+    );
+
+    return btnClasses;
+
+}
+
